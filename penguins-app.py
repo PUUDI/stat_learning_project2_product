177002c8da4edee4f9ae2a64c67d43d1821deb35
pulin disanayaka <pulindisanayaka@gmail.com>
2022-03-29 14:51:21 +0530

    Initial commit

---
+++ penguins-app.py
@@ -0,0 +1,160 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+#Css class for styling the app
+
+st.markdown("""
+<style>
+.app-header {
+    font-size:50px !important;
+    color: #F63366;
+    font-weight: 700;
+}
+.sidebar-header{
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    font-size: 28px;
+    letter-spacing: -1.2px;
+    word-spacing: 2px;
+    color: #FFFFFF;
+    font-weight: 700;
+    text-decoration: none;
+    font-style: normal;
+    font-variant: normal;
+    text-transform: capitalize;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="app-header">Dementia Prediction app</p>', unsafe_allow_html=True)
+
+expander = st.expander("Insight to the Project")
+expander.write("""
+    Original source of the data we used for the development of this website
+""")
+
+expander.write("""
+
+
+This app predicts the **Dementia** of patients!
+
+Data obtained from [Kaggle Website](https://www.kaggle.com/sid321axn/eda-for-predicting-dementia/data)
+""")
+
+
+# st.set_page_config(layout="wide")
+
+#Css Styling for the app
+
+
+
+st.sidebar.markdown('<p class="sidebar-header">Input the patient details</p>', unsafe_allow_html=True)
+
+# st.sidebar.markdown("""
+# [Example CSV input file](https://gist.githubusercontent.com/PUUDI/861771ffca8462507b487b6f75f2386d/raw/44e4760f1f6ee628c9674fe1c87e63bd4fbcf19d/gistfile1.txt)
+# """)
+
+# Collects user input features into dataframe
+# uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if 1>2:
+    a=5
+    # input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        
+        sex = st.sidebar.selectbox('Sex',('Male','Female'))
+        Age = st.sidebar.slider('Age', min_value =0,max_value =100,step =1)
+        EDUC = st.sidebar.slider('Educational Rating', min_value = 6,max_value = 23,step = 1)
+        SES = st.sidebar.slider('SES',min_value =1,max_value =5,step =1)
+        MMSE = st.sidebar.slider('MMSE', min_value =4,max_value =30,step =1)
+        # CDR = st.sidebar.slider('CDR', min_value =0.0,max_value =2.0,step =0.5)
+        eTIV = st.sidebar.slider('eTIV', min_value =1100,max_value =2010,step =1)
+        nWBV = st.sidebar.slider('MMSE', min_value =0.5,max_value =0.85,step =0.01)
+        ASF = st.sidebar.slider('ASF', min_value =0.875,max_value =1.6,step =0.01)
+        data = {'M/F': sex,
+                'Age': Age,
+                'EDUC': EDUC,
+                'SES': SES,
+                'MMSE': MMSE,
+                'eTIV':eTIV,
+                'nWBV':nWBV,
+                'ASF':ASF}
+        features = pd.DataFrame(data, index=[0])
+        return (features,sex,Age,EDUC,SES , MMSE, eTIV, nWBV, ASF)
+    input_df, sex,Age,EDUC,SES , MMSE, eTIV, nWBV, ASF = user_input_features()
+ 
+
+input_df['M/F'] = input_df['M/F'].apply(lambda x: ['Male', 'Female'].index(x))
+
+
+# Displays the user input features
+st.subheader('Predictor variables')
+
+
+# if uploaded_file is not None:
+#     st.write(input_df)
+
+    # st.write('Predictor variables')
+    # st.write(input_df)
+col1, col2, col3, col4 = st.columns(4)
+    
+col1.metric("Sex", sex)
+col2.metric("Age", Age)
+col3.metric("Edcation", EDUC)
+col4.metric("Social Status", SES)
+    
+col5, col6, col7, col8 = st.columns(4)
+
+col5.metric("MMSE", MMSE)
+col6.metric("eTIV", eTIV)
+col7.metric("Brain volume", nWBV)
+col8.metric("ASF", ASF)
+
+
+st.markdown("""---""")
+
+# Reads in saved classification model
+load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+
+# Apply model to make predictions
+prediction = load_clf.predict(input_df)
+prediction_proba = load_clf.predict_proba(input_df)
+
+#Creating the columns
+col1,col2 = st.columns(2)
+
+c1 = col1.container()
+c2 = col2.container()
+
+
+
+
+c1.subheader('Prediction')
+penguins_species = np.array(['Negative','Positive'])
+if prediction[0] == 0:
+    status = 'The Patient is Negative'
+else:
+    status = 'The Patient is positive with Dementia'
+
+c1.subheader(status)
+
+
+c2.subheader('Prediction Probability')
+# c2.write(prediction_proba)
+
+c2.metric(label="Risk of Dementia", value=round(prediction_proba[0][1],2))
+
+
+# go.Indicator(
+#             mode="gauge+number+delta",
+#             value=metrics["test_accuracy"],
+#             title={"text": f"Accuracy (test)"},
+#             domain={"x": [0, 1], "y": [0, 1]},
+#             gauge={"axis": {"range": [0, 1]}},
+#             delta={"reference": metrics["train_accuracy"]},
+#         )
---
+++ penguins-model-building.py
@@ -0,0 +1,97 @@
+import pandas as pd
+# penguins = pd.read_csv('penguins_cleaned.csv')
+dataset = pd.read_csv('https://gist.githubusercontent.com/PUUDI/861771ffca8462507b487b6f75f2386d/raw/44e4760f1f6ee628c9674fe1c87e63bd4fbcf19d/gistfile1.txt')
+classes = ['Nondemented', 'Demented']
+
+#dropping the converted Group data points
+dataset.drop(dataset.loc[dataset['Group'] == 'Converted'].index, inplace=True) # dataset.Group[dataset.Group == 'Converted'] = 'Nondemented' 
+
+#Dropping the unwanted columns that won't be needing to include in our model
+dataset.drop(['Subject ID', 'MRI ID', 'Hand','CDR','MR Delay','Visit'], axis=1, inplace=True) # 'MR Delay''Visit','Age','EDUC','eTIV'
+
+#Encoding binary variables
+dataset['M/F'] = dataset['M/F'].apply(lambda x: ['M', 'F'].index(x))
+
+#Encoding the class variable
+dataset['Class'] = [classes.index(group) for group in dataset['Group']]
+# Feature Encoding
+
+#Missing value Imputation
+from feature_engine.imputation import RandomSampleImputer
+#Imputing mean values for missing values
+# set up the imputer
+imputer = RandomSampleImputer(
+        random_state=['SES','MMSE'],
+        seed='observation',
+        seeding_method='add'
+    )
+
+# fit the imputer
+imputer.fit(dataset)
+
+dataset = imputer.transform(dataset)
+
+y = dataset.Class
+dataset.drop(['Group','Class'], axis=1, inplace=True) # 'MR Delay''Visit',
+X = dataset
+
+#Oversampling
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import SMOTE
+from numpy import where
+
+counter = Counter(y)
+print(counter)
+# transform the dataset
+oversample = SMOTE()
+X, y = oversample.fit_resample(X, y)
+# summarize the new class distribution
+counter = Counter(y)
+print(counter)
+
+from sklearn.model_selection import train_test_split
+train_feature, test_feature, train_label, test_label = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#Model fitting
+from sklearn.ensemble import RandomForestClassifier
+rf_final = RandomForestClassifier(bootstrap=False,max_depth=None,max_features='sqrt',min_samples_leaf=1,min_samples_split=2,n_estimators=750,random_state=40)
+rf_final.fit(train_feature, train_label)
+
+
+import pickle
+pickle.dump(rf_final, open('penguins_clf.pkl', 'wb'))
+
+
+
+
+
+# Ordinal feature encoding
+# # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+# df = penguins.copy()
+# target = 'species'
+# encode = ['sex','island']
+
+# for col in encode:
+#     dummy = pd.get_dummies(df[col], prefix=col)
+#     df = pd.concat([df,dummy], axis=1)
+#     del df[col]
+
+# target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+# def target_encode(val):
+#     return target_mapper[val]
+
+# df['species'] = df['species'].apply(target_encode)
+
+# # Separating X and y
+# X = df.drop('species', axis=1)
+# Y = df['species']
+
+# # Build random forest model
+# from sklearn.ensemble import RandomForestClassifier
+# clf = RandomForestClassifier()
+# clf.fit(X, Y)
+
+# Saving the model
+# import pickle
+# pickle.dump(clf, open('penguins_clf.pkl', 'wb'))
